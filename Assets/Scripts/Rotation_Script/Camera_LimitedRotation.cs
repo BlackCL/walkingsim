57cfa51dd6cee94cd328af0c8b42ead42313eedb
Yining <yl10231@nyu.edu>
2022-10-20 12:29:15 -0400

    camera rotation scene and changing script data

--- Assets/Scripts/Rotation_Script/Camera_LimitedRotation.cs
+++ Assets/Scripts/Rotation_Script/Camera_LimitedRotation.cs
@@ -6,8 +6,11 @@ public class Camera_LimitedRotation : MonoBehaviour
 {
 
     public float cameraSmoothingFactor = 1;
-    public float lookUpMax = 10;
+    public float lookUpMax = 20;
     public float lookUpMin = -50;
+
+    //public float lookLeftMax = 60;
+    //public float lookRightMax = -60;
 
 
     private Quaternion camRotation;
@@ -22,16 +25,17 @@ public class Camera_LimitedRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.GetAxis("Mouse X") + " : " + Input.GetAxis("Mouse Y"));
+        //Debug.Log(Input.GetAxis("Mouse X") + " : " + Input.GetAxis("Mouse Y"));
 
         camRotation.x += Input.GetAxis("Mouse Y") * cameraSmoothingFactor*(-1); //look up/down
         camRotation.y += Input.GetAxis("Mouse X") * cameraSmoothingFactor; //look left/right
 
         camRotation.x = Mathf.Clamp(camRotation.x, lookUpMin, lookUpMax); //clamping max and min look up/down
+       // camRotation.y = Mathf.Clamp(camRotation.y, lookLeftMax, lookRightMax); //limiting look left/right
 
         transform.localRotation = Quaternion.Euler(camRotation.x, camRotation.y, camRotation.z);
 
-        Debug.Log(camRotation.x);
+        //Debug.Log(camRotation.x);
 
         
     }
